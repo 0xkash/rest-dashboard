1f4de1c3d4a1c693e9c1e0e6404e5a218704a063
kash <kashvdmail@gmail.com>
2023-09-29 12:01:23 +0200

    Refs: #2 - General layout WIP (after crash)

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const site = {
+    name: 'rest/scraper',
+    description: 'A dashboard for the real estate scraper API.',
+    url: 'http://localhost:3000',
+    keywords: [
+        "real estate",
+        "scraper",
+        "rent scraper",
+        "real estate scraper",
+        "rent",
+        "rental",
+        "rental scraper",
+    ]
+}
