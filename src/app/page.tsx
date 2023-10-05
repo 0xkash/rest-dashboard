f284dd09ac2c53b67d05f3fd678d181b85c316ca
kash <kashvdmail@gmail.com>
2023-10-05 18:12:32 +0200

    Refs: #3 - Added shadecn data-table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { DataTable } from "@/components/data-table"
+
 export default function Index() {
   return (
     <main className="container flex min-h-screen flex-col py-6">
-      <p className="text-lg">Index page</p>
+      <DataTable />
     </main>
   )
 }
